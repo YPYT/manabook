8b6892e7ffe5b86fa742ad39c42d050baf8b0c0e
Yuka Toshima <yka723158@gmail.com>
2022-06-23 00:32:35 +1000

    make listing sold default:false

--- db/migrate/20220622142705_create_listings.rb
+++ db/migrate/20220622142705_create_listings.rb
@@ -6,7 +6,7 @@ class CreateListings < ActiveRecord::Migration[6.1]
       t.integer :price
       t.text :description
       t.integer :condition
-      t.boolean :sold
+      t.boolean :sold, default: false
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
       t.references :comment, null: false, foreign_key: true
