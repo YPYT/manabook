9a5efe6604667acfec436673840200b97d81f0d9
Yuka Toshima <yka723158@gmail.com>
2022-06-23 01:27:30 +1000

    add condition enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,5 +3,12 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_many :comments
 
-  
+  enum condition: {
+    new_or_unused: 1,
+    not_much_damege: 2,
+    a_little_damage: 3,
+    much_damage: 4,
+    overall_bad_condition: 5
+  }
+
 end
