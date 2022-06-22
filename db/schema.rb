806479e0092540a43929e495f007b5ec97c6ac00
Yuka Toshima <yka723158@gmail.com>
2022-06-23 00:39:05 +1000

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_142107) do
+ActiveRecord::Schema.define(version: 2022_06_22_142705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,23 @@ ActiveRecord::Schema.define(version: 2022_06_22_142107) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.string "list_image"
+    t.integer "price"
+    t.text "description"
+    t.integer "condition"
+    t.boolean "sold", default: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "comment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_listings_on_category_id"
+    t.index ["comment_id"], name: "index_listings_on_comment_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +103,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_142107) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "listings", "categories"
+  add_foreign_key "listings", "comments"
+  add_foreign_key "listings", "users"
 end
