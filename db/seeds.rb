284fa4c77645cae479e34ab541b230b51cba0f88
Yuka Toshima <yka723158@gmail.com>
2022-06-23 20:07:43 +1000

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# categories = ["a", "b", "c"]
+grades = ["Year 1 to 6", "Year 7 to 10", "Year 11, 12", "TAFE", "University"]
+subjects = ["English", "Second Languages", "Science", "Mathematics", "Technology", "The Arts", "Humanities", "Business Studies", "Health and Physical Education", "Accounting", "Aged Care", "Beauty", "Child Care", "Community Services", "Building & Constraction", "Business & Commerce", "Education & Teaching", "Arts", "Health", "IT", "Hospitality", "Sports", "Nursing","Criminology", "Engineering", "Music", "Medicine", "Law", "Others"]
 
-# if Category.count == 0
-#     categories.each do |category|
-#         Category.create(name: category)
-#         puts "created #{category} category"
-#     end
-# end
+if Category.count == 0
+    grades.each do |grade|
+        Category.create(name: grade)
+        puts "created #{grade} category"
+    end
+end
 
-# grade = Category.create(name: 'School Textbook')
-# year1_12, tafe, university = grade.children.create(
-#     [
-#         { name: 'Year 1 to Year 12'},
-#         { name: 'TAFE' },
-#         { name: 'University'}
-#     ]
-# )
-
-# ['English', 'Second Languages', 'Science', 'Mathematics', 'Technology', 'The Arts', 'Humanities', 'Business Studies', 'Health and Physical Education', 'Others'].each do |name|
-#     year1_12.children.create(name: name)
-# end
-
-# ['Accounting', 'Aged Care', 'Beauty', 'Child Care', 'Community Services', 'Byilding & Constraction', 'Education', 'Arts', 'Health', 'IT', 'Hospitality', 'Sports', 'Nursing','Others'].each do |name|
-#     tafe.children.create(name: name)
-# end
-
-# ['Accounting', 'Criminology', 'Engineering', 'IT', 'Business & Commerce', 'Education & Teaching', 'Health', 'Law', 'Science', 'Arts', 'Paramedicine', 'Sports', 'Music', 'Medicine', 'Others'].each do |name|
-#     university.children.create(name: name)
-# end
+if
+    subjects.each do |subject|
+        Category.create(name: subject)
+        puts "created #{subject} category"
+    end
+end
