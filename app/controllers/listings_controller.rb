035110e693b641db10f04476b390735f59247a92
Yuka Toshima <yka723158@gmail.com>
2022-06-23 19:37:14 +1000

    updated category table

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -34,6 +34,8 @@ class ListingsController < ApplicationController
     end
   end
 
+
+
   # PATCH/PUT /listings/1 or /listings/1.json
   def update
     respond_to do |format|
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,24 @@
 #         puts "created #{category} category"
 #     end
 # end
+
+# grade = Category.create(name: 'School Textbook')
+# year1_12, tafe, university = grade.children.create(
+#     [
+#         { name: 'Year 1 to Year 12'},
+#         { name: 'TAFE' },
+#         { name: 'University'}
+#     ]
+# )
+
+# ['English', 'Second Languages', 'Science', 'Mathematics', 'Technology', 'The Arts', 'Humanities', 'Business Studies', 'Health and Physical Education', 'Others'].each do |name|
+#     year1_12.children.create(name: name)
+# end
+
+# ['Accounting', 'Aged Care', 'Beauty', 'Child Care', 'Community Services', 'Byilding & Constraction', 'Education', 'Arts', 'Health', 'IT', 'Hospitality', 'Sports', 'Nursing','Others'].each do |name|
+#     tafe.children.create(name: name)
+# end
+
+# ['Accounting', 'Criminology', 'Engineering', 'IT', 'Business & Commerce', 'Education & Teaching', 'Health', 'Law', 'Science', 'Arts', 'Paramedicine', 'Sports', 'Music', 'Medicine', 'Others'].each do |name|
+#     university.children.create(name: name)
+# end
