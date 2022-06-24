3fb7e344fb2545c8e2678536d892dd2799c23672
Yuka Toshima <yka723158@gmail.com>
2022-06-24 20:21:20 +1000

    changed condition form by using helper file and added listing nav

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,8 @@
 module ListingsHelper
+    def format_condition(condition)
+        words = condition.split("_").map do |w|
+            w.capitalize
+        end
+        words.join(" ")
+    end
 end
