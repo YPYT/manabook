63cf845015b7795ebb3e6bec26bc0172f48317e4
Yuka Toshima <yka723158@gmail.com>
2022-06-26 20:10:47 +1000

    edit user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # get 'users/:id', to:"users#show", as: "users_show"
   # get 'users/:id/edit', to:"users#edit", as: "users_edit"
-  resources :users
+
 
   resources :listings
   root 'pages#home'
   devise_for :users
+  resources :users
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
