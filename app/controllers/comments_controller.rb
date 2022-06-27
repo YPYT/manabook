c360c61c70262838539b6e71192e3e5d5e0f4d21
Yuka Toshima <yka723158@gmail.com>
2022-06-27 11:06:30 +1000

    comment feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!  
+    
+    def create
+        @listing = Listing.find(params[:listing_id])
+        @comment = @listing.comments.create(comment_params)
+        if @comment.save
+            redirect_to listing_path(@listing), notice: 'Comment succeeded!'
+        else
+            flash[:alert] = 'Comment failed.'
+            render listing_path(@listing)
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        if @comment.destroy
+          redirect_to listing_path(@listing), notice: 'Deleted the comment'
+        else
+          flash[:alert] = 'Failed to delete comment.'
+          render listing_path(@listing)
+        end
+    end
+
+private
+    def comment_params
+        params.require(:comment).permit(:comment).merge(user_id: current_user.id, listing_id: params[:listing_id])
+    end
 end
