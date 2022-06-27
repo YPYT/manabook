63da8bae3d39552be546c45bf99d23daec991dde
Yuka Toshima <yka723158@gmail.com>
2022-06-27 14:06:04 +1000

    rails db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   has_many :listings
   has_many :comments, dependent: :destroy
+  
+  has_many :sold_orders, foreign_key: "seller_id", class_name: "Order"
+  has_many :bought_orders, foreign_key: "buyer_id", class_name: "Order"
 
   has_one_attached :profile_image
 end
