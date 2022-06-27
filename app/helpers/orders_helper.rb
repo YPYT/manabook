5451b4c50969c8ec1a793a7e1256be1fc2952d6b
Yuka Toshima <yka723158@gmail.com>
2022-06-27 23:24:16 +1000

    changed design of confirm page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,8 @@
 module OrdersHelper
+    def format_condition(condition)
+        words = condition.split("_").map do |w|
+            w.capitalize
+        end
+        words.join(" ")
+    end
 end
