32ea10ef11504a687248ec404d6445d04d4c6ecc
Yuka Toshima <yka723158@gmail.com>
2022-06-29 12:11:43 +1000

    removed config.active_storage.service = :amazon and changed config.active_storage.service = :local to config.active_storage.service = :amazon

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -77,6 +77,5 @@ Rails.application.configure do
   # add devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # save files in Amazon S3
-  config.active_storage.service = :amazon
+ 
 end
